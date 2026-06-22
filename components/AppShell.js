b8f1ac2b175bcf2b74908e0a6b45468a3d591ca6
augustocorazzarodrigues-collab <augustocorazzarodrigues@gmail.com>
2026-06-21 22:30:28 -0300

    Update AppShell.js

--- components/AppShell.js
+++ components/AppShell.js
@@ -2,13 +2,17 @@
 
 import Sidebar from './Sidebar';
 import AuthGate from './AuthGate';
+import AccessModeBanner from './AccessModeBanner';
 
 export default function AppShell({ children }) {
   return (
     <AuthGate>
       <div className="app-shell">
         <Sidebar />
-        <main className="main-area">{children}</main>
+        <main className="main-area">
+          <AccessModeBanner />
+          {children}
+        </main>
       </div>
     </AuthGate>
   );
